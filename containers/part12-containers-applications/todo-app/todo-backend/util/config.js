fd85f9680117254ef847655a3af1e8f986f0ceed
rahul-004x <142470408+rahul-004x@users.noreply.github.com>
2025-01-28 20:48:45 +0530

    upto p12.5

---
+++ containers/part12-containers-applications/todo-app/todo-backend/util/config.js
@@ -0,0 +1,7 @@
+const MONGO_URL = process.env.MONGO_URL || undefined
+const REDIS_URL = process.env.REDIS_URL || undefined
+
+module.exports = {
+  MONGO_URL,//: 'mongodb+srv://rahul004:kJLEUBLZiUF9Eb0C@full-stack.srh2y.mongodb.net/ToDo?retryWrites=true&w=majority',
+  REDIS_URL//: '//localhost:6378'
+}
